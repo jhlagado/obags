11b44fef125bcd912ac3c04f5a80c53caa3aed6c
John Hardy <john.hardy@workjam.com>
2021-02-09 16:52:17 +1100

    save

--- src/from-constant.test.ts
+++ src/from-constant.test.ts
@@ -3,14 +3,19 @@ import { CBFromConstant } from "./from-constant";
 import { pipe } from "./pipe";
 import { CBTake } from "./take";
 
+const times = 3;
+test('emit 3 numbers', () => {
+    const expected = [1000, 1000, 1000];
+    const expectedLength = expected.length;
+    const printOp = jest.fn((value: string) => {
+        console.log(value);
+        expect(value).toBe(expected.shift());
+    });
 
-const times = 10;
-test('make count up to 40 and print each number', () => {
-    const printOp = jest.fn((value: string) => console.log(value));
     pipe(
         new CBFromConstant(1000),
         (source) => new CBTake(source, times),
         (source) => new CBForEach(source, printOp)
     );
-    expect(printOp).toHaveBeenCalledTimes(times);
+    expect(printOp).toHaveBeenCalledTimes(expectedLength);
 })
